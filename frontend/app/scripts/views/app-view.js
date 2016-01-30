c53d159cc17e7a6fe66dd08a98e693c625d844ec
Marcos Bellucci <marcos@wyeworks.com>
2016-01-30 10:01:55 -0300

    Add application backbone view
    Re-render all the todo-list when any change occurs over app.todos

--- frontend/app/scripts/views/app-view.js
+++ frontend/app/scripts/views/app-view.js
@@ -1,17 +1,22 @@
-app.AppView = {
-  init : function() {
+app.AppView = Backbone.View.extend({
+
+  initialize: function() {
+    this.listenTo(app.todos, 'all', this.render)
+  },
+
+  render: function() {
+    $('.todo-list').html('');
     app.todos.each(function(todo) {
       var itemTemplate = window.JST["app/templates/item.html.jst"](todo.toJSON())
       $('.todo-list').append(itemTemplate)
     });
   }
-}
+});
 
 $(document).ready(function() {
   app.todos = new ItemCollection();
+  var App = new app.AppView;
   app.todos.add({ title: 'Pay bills', completed: true });
   app.todos.add({ title: 'Pay credit card', completed: false });
-
-  app.AppView.init();
 });
 
