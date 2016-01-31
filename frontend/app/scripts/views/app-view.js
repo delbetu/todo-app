4cb1541353be92c360926797d409e969449b1616
Marcos Bellucci <marcos@wyeworks.com>
2016-01-31 10:57:43 -0300

    New item creation

--- frontend/app/scripts/views/app-view.js
+++ frontend/app/scripts/views/app-view.js
@@ -1,7 +1,17 @@
+var ENTER_KEY = 13;
+
 AppView = Backbone.View.extend({
 
+  el: '.todoapp',
+
   initialize: function() {
+    this.newItemInput = this.$('input.new-todo');
+
     this.listenTo(todos, 'all', this.render)
+  },
+
+  events: {
+    'keypress .new-todo': 'createOnEnter'
   },
 
   render: function() {
@@ -10,6 +20,14 @@ AppView = Backbone.View.extend({
       var item = new ItemView({ model: todo });
       $('.todo-list').append(item.render().el)
     });
+  },
+
+  createOnEnter: function(e) {
+    if (e.which !== ENTER_KEY) return;
+    if (this.newItemInput.val() === '') return;
+
+    todos.add({ title: this.newItemInput.val() });
+    this.newItemInput.val('');
   }
 });
 
