b90fe02fd26afaf10ad3faa0acd2dbf3d3a41126
delbetu <delbetu@gmail.com>
2016-02-03 14:21:34 -0300

    Complete item API
    * CRUD for item resource.
    * Solve problem with Hashie parameters and rails 4.
    
    diff --git a/backend/.gitignore b/backend/.gitignore index e010d74..f9a0c42
    100644 --- a/backend/.gitignore +++ b/backend/.gitignore @@ -17,3 +17,4 @@
     /tmp
     **/.keep
     .byebug_history +*.swp diff --git a/backend/app/api/todos/api.rb
    b/backend/app/api/todos/api.rb index 596571a..4f70486 100644 ---
    a/backend/app/api/todos/api.rb +++ b/backend/app/api/todos/api.rb @@ -4,11 +4,62
    @@ module Todos
         format :json
         prefix :api
    
    + helpers do + def clean_params(params) +
    ActionController::Parameters.new(params) + end + end +
         resource :items do
           desc 'Returns all items'
           get do
             Item.all
           end + + desc 'Returns an item.' + params do + requires :id, type: Integer,
    desc: 'Item id.' + end + route_param :id do + get do + Item.find(params[:id]) +
    end + end + + desc 'Creates an item.' + params do + requires :title, type:
    String, desc: 'Your item title.' + end + post do + Item.create!({ + title:
    params[:title], + completed: false + }) + end + + desc 'Updates an existing
    item.' + params do + requires :id, type: Integer, desc: 'Item id.' + requires
    :item, type: Item + end + route_param :id do + put do + item_params =
    clean_params(params).require(:item).permit(:title, :completed) + + item =
    Item.find(params[:id]) + item.update(item_params) + end + end + + desc 'Deletes
    an item.' + params do + requires :id, type: Integer, desc: 'Item id to be
    deleted.' + end + route_param :id do + delete do + Item.find(params[:id]).destroy
    + end + end
         end
       end
     end diff --git a/backend/spec/api/v1/items_spec.rb
    b/backend/spec/api/v1/items_spec.rb index 20ae886..ba1651e 100644 ---
    a/backend/spec/api/v1/items_spec.rb +++ b/backend/spec/api/v1/items_spec.rb @@
    -10,4 +10,41 @@ describe "Todos API" do
         expect(response).to be_success
         expect(items.length).to eq(1)
       end + + it 'returns an item with the given id' do + Item.create(title: "Go to
    granny's home", completed: false) + + get '/api/v1/items/1' + + item =
    JSON.parse(response.body) + expect(response).to be_success +
    expect(item['id']).to eq(1) + end + + it 'creates an item' do + post
    '/api/v1/items', { title: "Go to granny's home" } + + json =
    JSON.parse(response.body) + expect(response).to be_success + item = Item.last +
    expect(item.title).to eq("Go to granny's home") + end + + it 'updates an existing
    item' do + Item.create(title: "Go to granny's home", completed: false) + + put
    '/api/v1/items/1', { item: { title: 'new title', completed: true } } + + item =
    Item.last + expect(item.title).to eq('new title') + expect(item.completed).to
    be_truthy + end + + it 'deletes an existing item' do + Item.create(title: "Go to
    granny's home", completed: false) + + delete '/api/v1/items/1' + +
    expect(Item.count).to eq(0) + end
     end

--- backend/app/api/todos/api.rb
+++ backend/app/api/todos/api.rb
@@ -4,10 +4,61 @@ module Todos
     format :json
     prefix :api
 
+    helpers do
+      def clean_params(params)
+        ActionController::Parameters.new(params)
+      end
+    end
+
     resource :items do
       desc 'Returns all items'
       get do
         Item.all
+      end
+
+      desc 'Returns an item.'
+      params do
+        requires :id, type: Integer, desc: 'Item id.'
+      end
+      route_param :id do
+        get do
+          Item.find(params[:id])
+        end
+      end
+
+      desc 'Creates an item.'
+      params do
+        requires :title, type: String, desc: 'Your item title.'
+      end
+      post do
+        Item.create!({
+          title: params[:title],
+          completed: false
+        })
+      end
+
+      desc 'Updates an existing item.'
+      params do
+        requires :id, type: Integer, desc: 'Item id.'
+        requires :item, type: Item
+      end
+      route_param :id do
+        put do
+          item_params = clean_params(params).require(:item).permit(:title, :completed)
+
+          item = Item.find(params[:id])
+          item.update(item_params)
+        end
+      end
+
+      desc 'Deletes an item.'
+      params do
+        requires :id, type: Integer, desc: 'Item id to be deleted.'
+      end
+      route_param :id do
+        delete do
+          Item.find(params[:id]).destroy
+        end
       end
     end
   end
--- backend/spec/api/v1/items_spec.rb
+++ backend/spec/api/v1/items_spec.rb
@@ -10,4 +10,41 @@ describe "Todos API" do
     expect(response).to be_success
     expect(items.length).to eq(1)
   end
+
+  it 'returns an item with the given id' do
+    Item.create(title: "Go to granny's home", completed: false)
+
+    get '/api/v1/items/1'
+
+    item = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(item['id']).to eq(1)
+  end
+
+  it 'creates an item' do
+    post '/api/v1/items', { title: "Go to granny's home" }
+
+    json = JSON.parse(response.body)
+    expect(response).to be_success
+    item = Item.last
+    expect(item.title).to eq("Go to granny's home")
+  end
+
+  it 'updates an existing item' do
+    Item.create(title: "Go to granny's home", completed: false)
+
+    put '/api/v1/items/1', { item: { title: 'new title', completed: true } }
+
+    item = Item.last
+    expect(item.title).to eq('new title')
+    expect(item.completed).to be_truthy
+  end
+
+  it 'deletes an existing item' do
+    Item.create(title: "Go to granny's home", completed: false)
+
+    delete '/api/v1/items/1'
+
+    expect(Item.count).to eq(0)
+  end
 end
