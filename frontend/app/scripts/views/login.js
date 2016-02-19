a9c9ee8dafef9947df0ae734daac4fc4d9f9ff6e
delbetu <delbetu@gmail.com>
2016-02-19 19:30:21 -0300

    Auth - Add call to login backend

--- frontend/app/scripts/views/login.js
+++ frontend/app/scripts/views/login.js
@@ -1,6 +1,37 @@
 var LoginView = Backbone.View.extend({
   template: JST["app/templates/login.html.jst"],
 
+  events: {
+    'click .button-container button': 'login'
+  },
+
+  login: function(e) {
+    e.preventDefault();
+
+    var credentials = { email: this.$('#email').val(), password: this.$('#password').val() };
+
+    $.ajax({
+      url: "http://localhost:3000/api/v1/user_session/",
+
+      type: 'POST', async: false,
+
+      dataType: 'json',
+
+      xhrFields: { withCredentials: true },
+
+      success: function(data) {
+        console.log(data.notice);
+        Backbone.history.navigate('');
+      },
+
+      error: function(data) {
+        console.log(data);
+      },
+
+      data: credentials
+    });
+  },
+
   render: function() {
     this.$el.html(this.template());
     return this;
