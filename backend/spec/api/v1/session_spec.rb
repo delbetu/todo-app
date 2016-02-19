8d89125b7f903e92be6eabc01f8ddad0d64b1596
delbetu <delbetu@gmail.com>
2016-02-19 11:35:28 -0300

    move session monkeypatch into a spec helper

--- backend/spec/api/v1/session_spec.rb
+++ backend/spec/api/v1/session_spec.rb
@@ -1,20 +1,6 @@
 require 'rails_helper'
 
 describe 'Use session API' do
-  before(:all) do
-    #TODO: Remove this monkeypatch
-    #https://github.com/rails/rails/issues/15843#issuecomment-125784043
-    if defined?(::ActionDispatch::Request::Session) &&
-        !::ActionDispatch::Request::Session.respond_to?(:each)
-      class ActionDispatch::Request::Session
-        def each(&block)
-          hash = self.to_hash
-          hash.each(&block)
-        end
-      end
-    end
-  end
-
   it 'login - logout' do
     user = User.create(email: 'user@test.com', password: 'test')
 
