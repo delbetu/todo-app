c584bd61b19db7bdd8630ce141a904160465c6a8
Marcos Bellucci <delbetu@gmail.com>
2016-02-20 17:43:00 -0300

    Complete group items api

--- backend/app/api/todos/api.rb
+++ backend/app/api/todos/api.rb
@@ -120,6 +120,49 @@ module Todos
         group_items = GroupItem.all
         present group_items
       end
+
+      desc 'Returns a group_item.'
+      params do
+        requires :id, type: Integer, desc: 'Group item id.'
+      end
+      get ':id'do
+        group_item = GroupItem.find(params[:id])
+        present group_item
+      end
+
+      desc 'Creates a group_item.'
+      params do
+        requires :group_item, type: Hash do
+          requires :list_title, type: String, desc: 'group_item title.'
+        end
+      end
+      post do
+        group_item = GroupItem.create!(params[:group_item])
+
+        present group_item
+      end
+
+      desc 'Updates an existing group_item.'
+      params do
+        requires :id, type: Integer, desc: 'Group item id.'
+        requires :group_item, type: Hash do
+          requires :list_title, type: String
+        end
+      end
+      put ':id' do
+        group_item = GroupItem.find(params[:id])
+        group_item.update(params[:group_item])
+        present group_item
+      end
+
+      desc 'Deletes a group_item.'
+      params do
+        requires :id, type: Integer, desc: 'Group item id to be deleted.'
+      end
+      delete ':id' do
+        group_item = GroupItem.find(params[:id]).destroy
+        present group_item
+      end
     end
   end
 end
