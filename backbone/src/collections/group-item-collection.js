ebc12565330d83554ac60e5c8409164580d33fad
M.Bellucci <delbetu@gmail.com>
2019-10-18 19:24:21 -0300

    Fix typo on api host

--- backbone/src/collections/group-item-collection.js
+++ backbone/src/collections/group-item-collection.js
@@ -2,7 +2,7 @@ import GroupItem from '../models/group-item-model.js'
 
 let GroupItemCollection = Backbone.Collection.extend({
   model: GroupItem,
-  url: process.apiHost+'/api/v1/group_items'
+  url: process.env.apiHost+'/api/v1/group_items'
 });
 
 export default GroupItemCollection
--- backbone/src/collections/item-collection.js
+++ backbone/src/collections/item-collection.js
@@ -3,7 +3,7 @@ import Item from '../models/item-model.js'
 let ItemCollection = Backbone.Collection.extend({
   model: Item,
   url: function() {
-    let url = process.apiHost+'/api/v1/group_items/';
+    let url = process.env.apiHost+'/api/v1/group_items/';
     url += this.group_item_id;
     url += '/items';
     return url;
