0cedee75641218df7ca35eed2a0a4fabb23ef39f
M.Bellucci <delbetu@gmail.com>
2019-10-18 19:23:41 -0300

    Add cors for production

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
+    origins 'http://localhost:8080', 'https://todo--backbone.herokuapp.com'
 
     resource '*',
       headers: :any,
