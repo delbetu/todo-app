9f21e79a8a47f0455efb4bed8f876b79beb0a2d6
M.Bellucci <delbetu@gmail.com>
2019-10-23 17:26:03 -0300

    Fix warning

--- api/app/core/list_groups.rb
+++ api/app/core/list_groups.rb
@@ -3,7 +3,7 @@ class ListGroups
   include C::Core
 
   Contract(C::KeywordArgs[user_id: C::Num, data_provider: C::RespondTo[:for]] => C::ArrayOf[Dto::Group] )
-  def self.call(user_id: user_id, data_provider:)
+  def self.call(user_id:, data_provider:)
     data_provider.for(user_id: user_id).map { |g| Dto::Group.new(g.id, g.list_title) }
   end
 end
